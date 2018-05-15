56340c9b3acc101c2ae06c526a895e8c39003b5f
ulysse <ulysse.fourquin>
2018-05-15 15:07:06 +0200

    df

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ var frameRateMonster = 800;
 var frameRateWeapon = 100;
 
 
-var eat = new Audio("eat.mp3");
+var touch = new Audio("touch.mp3");
 
 var gameElement = document.getElementById("game_area");
 var state_btn = document.getElementById("state_btn");
@@ -184,7 +184,7 @@ function checkCollapse(number){
     for(var i=0; i<monster.tabMonster.length; i++){
         if(weapon.tabWeapon[number].y<=monster.tabMonster[i].y){
             if(((weapon.tabWeapon[number].x)<=(monster.tabMonster[i].x+canvas.width*0.05))&&((weapon.tabWeapon[number].x)>=(monster.tabMonster[i].x))){
-                eat.play();
+                touch.play();
                 ctx.clearRect(weapon.tabWeapon[number].x, weapon.tabWeapon[number].y, 10, 40);
                 weapon.tabWeapon.splice(number,1);
                 monster.tabMonster[i].vie--;
